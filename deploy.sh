048209a56584b7dbee7b7d242d12b1e24517fb59
adamzwickey <azwickey@vmware.com>
2020-07-15 11:11:36 -0400

    initial checkin

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+: ${PARAMS_YAML?"Need to set PARAMS_YAML environment variable"}
+
+export TKG_LAB_SCRIPTS=tkg-lab/scripts
+
+# Management Cluster
+./scripts/mgmt.sh
+
+# Shared Services Cluster
+./scripts/shared.sh
+
+# Workload Cluster
+./scripts/workload.sh
